8436616a549954b60bb785be4972beb50fca2239
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2022-08-01 12:13:08 -0700

    Find Substring

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
+from re import sub
+
+
 def reverse_string(string):
-    reverse_string = ""
+    tempString = ""
     for s in string:
-        reverse_string = s + reverse_string
-    return reverse_string
+        tempString = s + tempString
+    return tempString
+
+
+def reverse_string_2(my_string):
+    my_string = list(my_string)
+    
+    i = 0
+    j = len(my_string) - 1
+    
+    while (i < j):
+        temp = my_string[j]
+        my_string[j] = my_string[i]
+        my_string[i] = temp
+        i += 1
+        j -= 1 
+        
+    return str(my_string)
+
+def find_substring(string, substring):
+    j = 0
+    for i in range(len(string)):
+        if string[i] == substring[j]:
+            for j in range(len(substring)):
+                if string[i] == substring[j]:
+                    if (j+1) == len(substring):
+                        if (i - j) < 0:
+                            return None
+                        return i - j
+                    
+
+ 
+            
+
 
 
 def main():
     print(reverse_string("Hello World"))
+    
+    print(reverse_string_2("Hello World"))
+    
+    print(find_substring("Hello World", "orld"))
 
 
 if __name__ == "__main__":
     main()
+
+
     
